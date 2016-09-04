925e800649ce49143774386a048f667edbc9e0ce
Amit Dube <amitdube41182@gmail.com>
2016-09-04 14:19:13 +0530

    Initial version of Add New Database html page

--- src/main/java/guru/springframework/controllers/LabDatabaseController.java
+++ src/main/java/guru/springframework/controllers/LabDatabaseController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,35 +13,35 @@ import guru.springframework.domain.LabDatabase;
 import guru.springframework.repositories.DatabaseRepository;
 
 @Controller
-@RequestMapping("/database")
 public class LabDatabaseController {
 
 	@Autowired
 	DatabaseRepository databaseRepository;
 
-	@RequestMapping(value = "", method = RequestMethod.GET)
+	@RequestMapping(value = "/database", method = RequestMethod.GET)
 	public String getDetails() {
 		return "database";
 	}
-
-	@RequestMapping(value="/read", method = RequestMethod.GET)
+	
+	@RequestMapping(value = "/viewAddNewDatabase", method = RequestMethod.GET)
+	public String addNewDatabase() {
+		return "addNewDatabase";
+	}
+	
+	@RequestMapping(value="/database/read", method = RequestMethod.GET)
 	@ResponseBody
 	public List<LabDatabase> read() {
+		System.out.println("inside database read");
 		return databaseRepository.findAll();
 	}
 
-	@RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+	@RequestMapping(value = "/database/delete", method = RequestMethod.DELETE)
 	@ResponseBody
 	public String delete(@RequestParam long updateId) {
 		LabDatabase labDatabase = databaseRepository.findOne(updateId);
 		databaseRepository.delete(labDatabase);
+		System.out.println("inside database delete");
 		return "success";
 	}
 	
-	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	@ResponseBody
-	public String add(@RequestBody LabDatabase labDatabase){
-		databaseRepository.save(labDatabase);
-		return "success";
-	}
 }
