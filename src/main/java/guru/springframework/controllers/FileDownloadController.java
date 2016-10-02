e16f51797a1c84a3e294f88615b38513063344b0
Amit Dube <amitdube41182@gmail.com>
2016-10-02 19:24:22 +0530

    Fix to download database file

--- src/main/java/guru/springframework/controllers/FileDownloadController.java
+++ src/main/java/guru/springframework/controllers/FileDownloadController.java
@@ -1,6 +1,5 @@
 package guru.springframework.controllers;
 
-import java.io.IOException;
 import java.io.InputStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +9,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import guru.springframework.domain.Lab;
+import guru.springframework.domain.LabDatabase;
+import guru.springframework.repositories.DatabaseRepository;
 import guru.springframework.services.S3StorageService;
 
 @Controller
@@ -22,16 +24,30 @@ public class FileDownloadController {
 	@Autowired
 	S3StorageService s3StorageService;
 	
-	@RequestMapping(value = "/download/{fileName}.{ext}", method = RequestMethod.GET)
-	public ResponseEntity<InputStreamResource> downloadFileDirectlyFromS3(@PathVariable String fileName, @PathVariable String ext)
-	                                                                  throws IOException {
-		
-		InputStream inputStream = s3StorageService.download(fileName + "." + ext, "databases");
-	    HttpHeaders respHeaders = new HttpHeaders();
-	    respHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-	    respHeaders.setContentDispositionFormData("attachment", fileName + "." + ext);
+	@Autowired
+	DatabaseRepository databaseRepository;
+	
+	@RequestMapping(value = "/downloadDatabaseFiles", method = RequestMethod.GET)
+	public ResponseEntity<InputStreamResource> downloadDatabaseFiles(@RequestParam(name="database_id") String database_id)
+	                                                                  throws Exception {
+		LabDatabase labDatabase = databaseRepository.findOne(Long.parseLong(database_id));
+		if(labDatabase != null){
+			Lab lab = labDatabase.getLab();
+			if(lab != null){
+				String fileName = labDatabase.getFilePath();
+				String folderName = lab.getLabName() + S3StorageService.SUFFIX + labDatabase.getUser().getUsername() + S3StorageService.SUFFIX + S3StorageService.DATABASE_FOLDER;
+				InputStream inputStream = s3StorageService.download(fileName, folderName);
+			    HttpHeaders respHeaders = new HttpHeaders();
+			    respHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+			    respHeaders.setContentDispositionFormData("attachment", fileName);
 
-	    InputStreamResource isr = new InputStreamResource(inputStream);
-	    return new ResponseEntity<InputStreamResource>(isr, respHeaders, HttpStatus.OK);
+			    InputStreamResource isr = new InputStreamResource(inputStream);
+			    return new ResponseEntity<InputStreamResource>(isr, respHeaders, HttpStatus.OK);
+			}
+			
+		}
+		throw new Exception("Unable to find request database file.");
 	}
+	
+	
 }
